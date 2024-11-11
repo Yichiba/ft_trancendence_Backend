732a396f5269ebb37139e93667c25ffe61147a19
Achraf Toukmati <atoukmat@c2r11p2.1337.ma>
2024-11-11 01:11:45 +0100

    good trip

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-python3 -m venv .env
-source ./env/bin/activate
-pip3 install authentication/requirement_home.txt
+if [ -d ".venv" ]; then
+    echo "env exist"
+    source .venv/bin/activate
+else
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip3 install -r authentication/requirement_home.txt
+fi
+
 python3 authentication/manage.py  runserver
